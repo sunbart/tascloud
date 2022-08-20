b818d7c931aab59ed5a9a2af584e0ffa340fbeec
sunbart <jan@bartonicek.dev>
2022-08-20 01:16:48 +0000

    First index page

--- tascloud/views.py
+++ tascloud/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(render_to_string("tascloud/index.html"))
